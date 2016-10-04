7ca62854b71d9aad81a77d3f479770227c3484b5
DidrikKvanvik <dkv001@student.uib.no>
2016-10-04 13:27:07 +0200

    Add getAllCurrentBids, getAuctions and getAuctionUserId()

--- src/main/java/boundary/AuctionUserFacade.java
+++ src/main/java/boundary/AuctionUserFacade.java
@@ -11,6 +11,7 @@ import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
 import javax.ejb.Stateless;
+import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.joda.time.DateTime;
@@ -38,24 +39,51 @@ public class AuctionUserFacade extends AbstractFacade<AuctionUser> {
     }
     
     /**
+     * Method to retrieve auction_user_id
+     * @return id
+     *         auction_user_id
+     *      
+     */
+    public int getAuctionUserId(){
+        String username = FacesContext.getCurrentInstance().getExternalContext().getRemoteUser();
+        int id = em.createQuery(
+                "SELECT a.auction_user_id FROM auction_user AS a WHERE a.auction_user_id = " + username
+        ).setMaxResults(1).getFirstResult();
+        return id;
+    }
+    
+    /**
      * Method returns all the winning auctions of a given customer.
-     * @param auctionUser
-     *          customer
      * @return winningList
      *          LinkedList of winning auctions
      * 
      */
-    public List getAllWinningAuctions(AuctionUser auctionUser){
+    public List getAllWinningAuctions(){
+        return getAuctions(true);
+    }
+    
+    /**
+     * Method returns all the current auctions of a given customer
+     * @return auctionList
+     *          LinkedList of current bids on auctions
+     */
+    public List getAllCurrentBids(){
+       return getAuctions(false);
+    }
+    
+    
+    public List getAuctions(boolean isOver){
+        int id = getAuctionUserId();
         LinkedList<Integer> list = new LinkedList<Integer>();
-        LinkedList<Auction> winningList = new LinkedList<Auction>();
+        LinkedList<Auction> auctionList = new LinkedList<Auction>();
         Auction auction;
         DateTime nowDate = new DateTime();
         DateTime dateTime = new DateTime();
         
-        list.addAll(em.createQuery( //query to retrieve all bids of a customer
+        list.addAll(em.createQuery( //query to retrieve all bids
                 "SELECT a.auction_id FROM auction AS a, auction_user AS au, bid AS b"
                         + " WHERE a.auction_id = b.auction_id AND b.auction_user_id = " + 
-                        auctionUser.getId() + " AND a.bid_id = b.bid_id").getResultList());
+                        id + " AND a.bid_id = b.bid_id").getResultList());
         
         //Adding winning auctions to the winningList
         for(int i = 0; i < list.size(); i++){
@@ -63,11 +91,18 @@ public class AuctionUserFacade extends AbstractFacade<AuctionUser> {
            dateTime = new DateTime(auction.getStartTime());
            dateTime.plusSeconds(auction.getDuration().intValue());
            
-           if(dateTime.compareTo(nowDate) < 0){
-               winningList.add(auction);
+           if(isOver){
+                if(dateTime.compareTo(nowDate) < 0){
+                    auctionList.add(auction);
+                }
+           }else{
+               if(dateTime.compareTo(nowDate) > 0){
+                    auctionList.add(auction);
+                }
            }
-       
         }
-        return winningList;
+        return auctionList;
+    
     }
+    
 }
