d469ea243eab7dd1378523fa8a10bb26e7f369ec
DidrikKvanvik <dkv001@student.uib.no>
2016-10-04 13:06:59 +0200

    Add getAllWinningAuctions method to retrieve all winning auctions for a usere

--- src/main/java/boundary/AuctionUserFacade.java
+++ src/main/java/boundary/AuctionUserFacade.java
@@ -5,10 +5,18 @@
  */
 package boundary;
 
+import entities.Auction;
 import entities.AuctionUser;
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.Instant;
+import org.joda.time.Interval;
 
 /**
  *
@@ -29,4 +37,37 @@ public class AuctionUserFacade extends AbstractFacade<AuctionUser> {
         super(AuctionUser.class);
     }
     
+    /**
+     * Method returns all the winning auctions of a given customer.
+     * @param auctionUser
+     *          customer
+     * @return winningList
+     *          LinkedList of winning auctions
+     * 
+     */
+    public List getAllWinningAuctions(AuctionUser auctionUser){
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        LinkedList<Auction> winningList = new LinkedList<Auction>();
+        Auction auction;
+        DateTime nowDate = new DateTime();
+        DateTime dateTime = new DateTime();
+        
+        list.addAll(em.createQuery( //query to retrieve all bids of a customer
+                "SELECT a.auction_id FROM auction AS a, auction_user AS au, bid AS b"
+                        + " WHERE a.auction_id = b.auction_id AND b.auction_user_id = " + 
+                        auctionUser.getId() + " AND a.bid_id = b.bid_id").getResultList());
+        
+        //Adding winning auctions to the winningList
+        for(int i = 0; i < list.size(); i++){
+           auction = em.find(Auction.class, list.get(i));
+           dateTime = new DateTime(auction.getStartTime());
+           dateTime.plusSeconds(auction.getDuration().intValue());
+           
+           if(dateTime.compareTo(nowDate) < 0){
+               winningList.add(auction);
+           }
+       
+        }
+        return winningList;
+    }
 }
