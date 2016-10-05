2c8e10c9e7d9c720ce7a4a237d964467106eda55
Andreas <>
2016-10-05 10:29:00 +0200

    added search mehtods in AuctionFacade

--- src/main/java/boundary/AuctionFacade.java
+++ src/main/java/boundary/AuctionFacade.java
@@ -5,7 +5,11 @@
  */
 package boundary;
 
+import com.sun.javafx.scene.control.skin.VirtualFlow;
 import entities.Auction;
+import enums.Category;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -29,4 +33,37 @@ public class AuctionFacade extends AbstractFacade<Auction> {
         super(Auction.class);
     }
     
+    public List<Auction> getAuctionByCategory(Category category){
+        List<Auction> tempList= new ArrayList<>();
+        for(int i=0;i<findAll().size();i++){
+            if(findAll().get(i).getProduct().getCategory()==category){
+                tempList.add(findAll().get(i));
+            }
+        }
+        return tempList;
+    }
+    
+    public List<Auction> getAuctionsByKeyword(String keyword){
+        List<Auction> tempList= new ArrayList<>();
+        for(int i=0;i<findAll().size();i++){
+            if(findAll().get(i).getProduct().getProductName().contains(keyword)
+                || findAll().get(i).getProduct().getDescription().
+                        contains(keyword)){                
+                tempList.add(findAll().get(i));
+            }
+        }
+        return tempList;
+    }
+    
+   public List<Auction> getAuctionsByKeywordAndCategory(String keyword, Category
+           category){
+       List<Auction> tempList= getAuctionByCategory(category);
+       for(int i=0; i<tempList.size(); i++){
+           if(!tempList.get(i).getProduct().getProductName().contains(keyword)||
+                !tempList.get(i).getProduct().getDescription().contains(keyword)){
+               tempList.remove(i);
+           }
+       }
+       return tempList;
+   }
 }
