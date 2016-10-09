fac8d2e1cb0ec83f0be43402d10240fd5e011a0c
andrmos <mossige.a@gmail.com>
2016-10-09 17:14:23 +0200

    Add category and checkbox field in new auction view

--- src/main/java/manageBeans/AddAuctionMB.java
+++ src/main/java/manageBeans/AddAuctionMB.java
@@ -7,9 +7,12 @@ package manageBeans;
 
 import boundary.AuctionFacade;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.model.SelectItem;
 
 /**
  *
@@ -26,6 +29,8 @@ public class AddAuctionMB implements Serializable {
     private String description;
     private int price;
     private int duration;
+    private String selectedCategory;
+    private boolean published;
 
     /**
      * Creates a new instance of AddAuctionMB
@@ -64,9 +69,37 @@ public class AddAuctionMB implements Serializable {
     public void setDuration(int duration) {
         this.duration = duration;
     }
+
+    public String getSelectedCategory() {
+        return selectedCategory;
+    }
+
+    public void setSelectedCategory(String selectedCategory) {
+        this.selectedCategory = selectedCategory;
+    }
+
+    public boolean isPublished() {
+        return published;
+    }
+
+    public void setPublished(boolean published) {
+        this.published = published;
+    }
+    
+     public List<SelectItem> getSelectedCategories(){
+        List selectedCategories=new ArrayList();
+        selectedCategories.add(new SelectItem("0", "Clothing"));
+        selectedCategories.add(new SelectItem("1", "Electronics"));
+        selectedCategories.add(new SelectItem("2", "White Wear"));
+        selectedCategories.add(new SelectItem("3", "Jewlerry"));        
+        selectedCategories.add(new SelectItem("4", "Guns"));        
+        return selectedCategories;
+    }
     
     public void addAuction() {
-        System.out.println("title: " + title);
+        
+        System.out.println("test");
+        
     }
     
     
