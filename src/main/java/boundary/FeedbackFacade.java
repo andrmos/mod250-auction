80b7502fdbf54327f0da0c7086f82c9c45e35a35
DidrikKvanvik <dkv001@student.uib.no>
2016-10-10 10:14:09 +0200

    Add checkforexistingfeedback method

--- src/main/java/boundary/FeedbackFacade.java
+++ src/main/java/boundary/FeedbackFacade.java
@@ -7,6 +7,7 @@ package boundary;
 
 import entities.Auction;
 import entities.Feedback;
+import java.util.LinkedList;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -32,5 +33,33 @@ public class FeedbackFacade extends AbstractFacade<Feedback> {
     
     public void createFeedback(Feedback feedback){
         em.persist(feedback);
+    }
+    
+    /**
+     * Gets all existing feedbacks
+     * @return 
+     *          list of feedbacks
+     */
+    public LinkedList getAllFeedbacks(){
+        LinkedList<Feedback> feedbacks = new LinkedList<Feedback>();
+        feedbacks.addAll(this.findAll());
+        return feedbacks;
+    }
+    
+    /**
+     * Checks if a given auction have an existing feedback
+     * @param auction 
+     *          the auction to check
+     * @return boolean
+     *          true if feedbacks exists, false else
+     */
+    public boolean checkForExistingFeedback(Auction auction){
+        LinkedList<Feedback> feedbacks = getAllFeedbacks();
+        for(int i = 0; i < feedbacks.size(); i++){
+            if(auction.getId().equals(feedbacks.get(i).getId())){
+                return true; //This auction have a feedback
+            }  
+        }
+        return false; //there is no feedback
     }
 }
