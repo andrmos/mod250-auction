18c274af99d4d987a17fe594a35990ab8dc9c110
DidrikKvanvik <dkv001@student.uib.no>
2016-10-12 10:38:15 +0200

    Add javadoc to methods and removed unused imports

--- src/main/java/support/AuctionSupport.java
+++ src/main/java/support/AuctionSupport.java
@@ -5,12 +5,8 @@
  */
 package support;
 
-import boundary.AuctionFacade;
-import boundary.AuctionUserFacade;
 import entities.Auction;
-import entities.AuctionUser;
 import entities.Bid;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import org.joda.time.DateTime;
@@ -21,38 +17,67 @@ import org.joda.time.Seconds;
  * @author Andreas
  */
 public class AuctionSupport {
+    
+    /**
+     * Checks if an auction is finished
+     * @param auction
+     *          auction to check
+     * @return boolean
+     *          true if finished, else false
+     */
     public static boolean isAuctionFinished(Auction auction){           
        return auction.isPublished() && (new DateTime(auction.getStartTime()).
                plusSeconds(auction.getDuration().intValue()).compareTo(new DateTime())<0);
-   }
+    }
     
-   public static int secondsToAuctionIsFinished(Auction auction){
-      if(auction.getStartTime()==null){
-          return 0;
-      }
-      else if(auction.getStartTime()!=null){
-          DateTime finsihedDate = new DateTime(auction.getStartTime()).
-                  plusSeconds(auction.getDuration().intValue());
-          DateTime now = new DateTime();
-          if (finsihedDate.isAfter(now)) {
-              return Seconds.secondsBetween(now,finsihedDate).getSeconds();
-          }
-      }            
-      return 0;
-   }
+    /**
+     * Returns number of seconds left until an auction is finished
+     * @param auction
+     *          auction to check
+     * @return int
+     *          number of seconds left
+     */
+    public static int secondsToAuctionIsFinished(Auction auction){
+        if(auction.getStartTime()==null){
+            return 0;
+        }
+        else if(auction.getStartTime()!=null){
+            DateTime finsihedDate = new DateTime(auction.getStartTime()).
+                plusSeconds(auction.getDuration().intValue());
+            DateTime now = new DateTime();
+            if (finsihedDate.isAfter(now)) {
+                return Seconds.secondsBetween(now,finsihedDate).getSeconds();
+            }
+        }            
+        return 0;
+    }
+    
+    /**
+     * Sorts a list of auctions based on the time left
+     * @param auctions
+     *          list of auctions
+     * @return auctions
+     *          sorted aucitons list
+     */
+    public static List<Auction> sortAuctionsBasedOnTime(List<Auction> auctions) {
+        Collections.sort(auctions);       
+        return auctions;
+    }
    
-   public static List<Auction> sortAuctionsBasedOnTime(List<Auction> auctions) {
-       Collections.sort(auctions);       
-       
-       return auctions;
-   }
-   
-   public static double getCurrentPrice(Auction auction){
+    /**
+     * Returns the current price of an auction, that is the highest bid if
+     * it has one, else the initialized price of the auction
+     * @param auction
+     *          auction to get the price of
+     * @return price
+     *          the price of the auction
+     */
+    public static double getCurrentPrice(Auction auction){
         Bid bid = auction.getBid();
         if(bid == null){
-          return auction.getInitPrice();
+            return auction.getInitPrice();
         }else{
             return bid.getAmount();
         }
-   }
+    }
 }
