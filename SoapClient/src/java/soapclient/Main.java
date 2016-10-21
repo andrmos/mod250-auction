c1ff541ec22b4bb787fc057a46c30037c9d4bec1
Ravenwalley <a.ravndal91@gmail.com>
2016-10-21 13:30:25 +0200

    Enterprise Client now get access to soapservices. Also deleted https for soapservice because it fucked up the connection between the client and service

--- SoapClient/src/java/soapclient/Main.java
+++ SoapClient/src/java/soapclient/Main.java
@@ -5,17 +5,35 @@
  */
 package soapclient;
 
+import java.util.Arrays;
+import javax.xml.ws.WebServiceRef;
 /**
+ *
+ * @author Eier
+ */
+import webservices.SoapService_Service;/**
  *
  * @author Eier
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    @WebServiceRef(wsdlLocation = "http://localhost:8080/mod250_auction/SoapService?WSDL")
+    private static SoapService_Service service;
+
+       
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println(activeAucitons().get(0).getInitPrice());
+        
     }
+
+    private static java.util.List<webservices.Auction> activeAucitons() {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        webservices.SoapService port = service.getSoapServicePort();
+        return port.activeAucitons();
+    }
+
+
     
 }
